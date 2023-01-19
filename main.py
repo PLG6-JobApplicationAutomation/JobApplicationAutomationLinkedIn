38da18e0fc7c3ce97bbf5177c0661126886c0f11
Jayden Colmer <119273629+jayColmer19@users.noreply.github.com>
2023-01-19 12:42:07 +1030

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import re
